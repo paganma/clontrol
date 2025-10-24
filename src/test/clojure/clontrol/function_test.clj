e3f7cfcf08fcfc5aef940b409733b04e12f90f48
Marco Paganoni <paganma@usi.ch>
2025-10-24 14:30:24 +0200

    feat(function): Implement call-shift

--- src/test/clojure/clontrol/function_test.clj
+++ src/test/clojure/clontrol/function_test.clj
@@ -8,9 +8,124 @@
     :refer [defn-shift
             fn-shift]]
    [clontrol.function.shifter
-    :refer [invoke-shift]]))
+    :refer [apply-shift
+            call-shift
+            invoke-shift]]))
 
 (deftest fn-cc-test
+  (testing "Multiple Arities"
+    (is (= (invoke-shift
+            (fn-shift [x] x)
+            identity 1)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y] x)
+            identity 1 2)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z] x)
+            identity 1 2 3)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a] x)
+            identity 1 2 3 4)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a b] x)
+            identity 1 2 3 4 5)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a b c] x)
+            identity 1 2 3 4 5 6)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a b c d] x)
+            identity 1 2 3 4 5 6 7)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a b c d e] x)
+            identity 1 2 3 4 5 6 7 8)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a b c d e f] x)
+            identity 1 2 3 4 5 6 7 8 9)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a b c d e f g] x)
+            identity 1 2 3 4 5 6 7 8 9 10)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a b c d e f g h] x)
+            identity 1 2 3 4 5 6 7 8 9 10 11)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a b c d e f g h i] x)
+            identity 1 2 3 4 5 6 7 8 9 10 11 12)
+           1))
+    (is (= (invoke-shift
+            (fn-shift [x y z a b c d e f g h i l] x)
+            identity 1 2 3 4 5 6 7 8 9 10 11 12 13)
+           1))
+    (is (= (apply-shift
+            (fn-shift [x y z a b c d e f g h i l p1 p2 p3 p4 p5 p6] x)
+            identity (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19))
+           1))
+    (is (= (call-shift
+            (fn-shift [x] x)
+            identity 1)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y] x)
+            identity 1 2)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z] x)
+            identity 1 2 3)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a] x)
+            identity 1 2 3 4)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b] x)
+            identity 1 2 3 4 5)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b c] x)
+            identity 1 2 3 4 5 6)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b c d] x)
+            identity 1 2 3 4 5 6 7)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b c d e] x)
+            identity 1 2 3 4 5 6 7 8)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b c d e f] x)
+            identity 1 2 3 4 5 6 7 8 9)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b c d e f g] x)
+            identity 1 2 3 4 5 6 7 8 9 10)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b c d e f g h] x)
+            identity 1 2 3 4 5 6 7 8 9 10 11)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b c d e f g h i] x)
+            identity 1 2 3 4 5 6 7 8 9 10 11 12)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b c d e f g h i l] x)
+            identity 1 2 3 4 5 6 7 8 9 10 11 12 13)
+           1))
+    (is (= (call-shift
+            (fn-shift [x y z a b c d e f g h i l p1 p2 p3 p4 p5 p6] x)
+            identity 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
+           1)))
   (testing "Destructuring Arguments"
     (is (= (invoke-shift
             (fn-shift [{x :x}] x)
@@ -88,7 +203,20 @@
          java.lang.AssertionError
          (invoke-shift
           (fn-shift [x] {:pre [false] :post [false]} x)
-          identity true)))))
+          identity true))))
+(testing "Error handling"
+    (is (thrown?
+         clojure.lang.ExceptionInfo
+         (eval `((fn-shift [])))))
+    (is (thrown?
+         clojure.lang.ExceptionInfo
+         (eval `((fn-shift [p1#]) 1))))
+    (is (thrown?
+         clojure.lang.ExceptionInfo
+         (eval `((fn-shift [p1# p2#]) 1 2))))
+    (is (thrown?
+         clojure.lang.ExceptionInfo
+         (eval `((fn-shift [p1# p2# p3#]) 1 2 3))))))
 
 (deftest defn-cc-test
   (testing "Well formed functions"
