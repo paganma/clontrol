c2e11cb60dd30a95fbf65c9554de982bc6c34a07
Marco Paganoni <paganma@usi.ch>
2026-01-05 18:18:37 +0100

    chore(core-test): Refactor tests in operator-test into core-test

--- src/test/clojure/clontrol/operator_test.clj
+++ src/test/clojure/clontrol/operator_test.clj
@@ -2,8 +2,6 @@
   (:require
    [clojure.test
     :refer [deftest is testing]]
-   [clontrol.function
-    :refer [fn-shift]]
    [clontrol.operator
     :refer [abort
             reset
@@ -24,61 +22,33 @@
 
 (deftest operator-test
 
-  (testing "Generator"
-    (letfn [(yield [k x] [k x])
-            (step [[k x]] (k x))
-            (fetch [[_ x]] x)]
-      (let [generator
-            (reset
-             (loop [a 0]
-               (shift yield a)
-               (recur (+ a 1))))]
-        (is (= (fetch (step generator)) 1))
-        (is (= (fetch (step (step generator))) 2)))))
-
-  (testing "CC Generator"
-    (let [yield
-          (fn-shift
-           [x]
-           (shift (fn [k] [k x])))]
-      (letfn
-       [(step [[k x]] (k x))
-        (fetch [[_ x]] x)]
+  (testing "Examples"
+    (testing "Generator"
+      (letfn [(yield [k x] [k x])
+              (step [[k x]] (k x))
+              (fetch [[_ x]] x)]
         (let [generator
               (reset
-               (loop [a 0]
-                 (yield a)
-                 (recur (+ a 1))))]
+                (loop [a 0]
+                  (shift yield a)
+                  (recur (+ a 1))))]
           (is (= (fetch (step generator)) 1))
-          (is (= (fetch (step (step generator))) 2))))))
+          (is (= (fetch (step (step generator))) 2)))))
 
-  (testing "Stateful Generator"
-    (letfn [(yield [k x]
-              (reset-to k (shift (fn [k] [k x]))))]
-      (let [fetch (fn [t] (second @t))
-            step (fn [t] (reset! t ((first @t) (second @t))))
-            generator
-            (atom
-             (reset
-              (loop [x 0]
-                (recur (shift yield (inc x))))))]
-        (dotimes [_ 10000]
-          (step generator))
-        (is (= (fetch generator) 10001)))))
+    (testing "Stateful Generator"
+      (letfn [(yield [k x]
+                (reset-to k (shift (fn [k] [k x]))))]
+        (let [fetch (fn [t] (second @t))
+              step (fn [t] (reset! t ((first @t) (second @t))))
+              generator
+              (atom
+               (reset
+                 (loop [x 0]
+                   (recur (shift yield (inc x))))))]
+          (dotimes [_ 10000]
+            (step generator))
+          (is (= (fetch generator) 10001))))))
 
-  (testing "Stateful Generator with Handlers"
-    (let [yield (fn-shift [x] (shift (fn [k] [k x])))
-          fetch (fn [t] (second @t))
-          step (fn [t] (reset! t ((first @t) (second @t))))
-          generator
-          (atom
-           (reset
-            (loop [x 0]
-              (yield (+ x 1))
-              (recur (inc x)))))]
-      (dotimes [_ 10000]
-        (step generator))
-      (is (= (fetch generator) 10001))))
 
   (testing "Direct recur in CPS loop"
     (reset
@@ -154,13 +124,6 @@
       (is (= (reset (+ 1 (shift (fn [k] (+ (k 1) (k 2)))) (shift divide 3 3))) 7))
 
       (is (= (reset (shift fib 10)) 55))))
-
-  (testing "Composability"
-    (is (let [a
-              (fn-shift
-               [x]
-               (shift (fn [k] [(k 1) (k 2)])))]
-          (= (reset (a 1)) [1 2]))))
 
   (testing "Shift in CASE"
     (is (= (reset
@@ -631,94 +594,6 @@
      (let [m (atom nil)]
        (locking (shift (fn [k] (k m)))
          (is (swap! m not))))))
-
-  (testing "Local Handler Declaration"
-    (is (= (let [f (fn-shift [a] (shift (fn [k] (k [(+ a 1) (+ a 2)]))))]
-             (reset
-              (f 1)))
-           [2 3]))
-
-    (is (= (reset
-            (let [f (fn-shift
-                     [a]
-                     (shift (fn [k]
-                              [(k (+ a 1)) (k (+ a 2))])))]
-              (+ 1 (f 1))))
-           [3 4]))
-
-    (is (= (reset
-            (let [f (fn-shift
-                     [a]
-                     (let [g (fn-shift [x] (shift (fn [_] x)))]
-                       (shift
-                        (fn [k]
-                          [(k (+ a 1)) (k (+ a 2))]))
-                       (g 2)))]
-              (+ 1 (f 1))))
-           [2 2]))
-    (is (= (reset
-            (let [f (fn-shift [_] (shift (fn [_] 3)))]
-              (if (> (shift (fn [k] (k 11))) 10)
-                (do (f 2) 1)
-                1)))
-           3))
-    (is (= (reset
-            (let [f (fn-shift [_] (shift (fn [_] 3)))]
-              (if (> (shift (fn [k] (k 11))) 10)
-                (do (f 2) 1)
-                1)))
-           3))
-    (is (= (reset
-            (let [f (fn-shift
-                     [_]
-                     (shift (fn [_] 3))
-                       ;; Unreachable
-                     (recur 1))]
-              (if (> (shift (fn [k] (k 11))) 10)
-                (do (f 2) 1)
-                1)))
-           3)))
-
-  (testing "Looping Handler"
-    (is (=
-         (reset
-          (let [count-down
-                (fn-shift
-                 [x]
-                 (if (> x 0)
-                   (recur (dec x))
-                   x))]
-            (count-down 10)))
-         0))
-    (is (=
-         (reset
-          (let [count-down
-                (fn-shift
-                 [x]
-                 (if (> x 0)
-                   (recur (dec x))
-                   x))]
-            (count-down 10)))
-         0))
-    (is (=
-         (reset
-          (let [count-down
-                (fn-shift
-                 [x]
-                 (if (> x 0)
-                   (recur (shift (fn [k] (k (dec x)))))
-                   x))]
-            (count-down 10)))
-         0))
-    (is (=
-         (reset
-          (let [g (fn-shift
-                   [x]
-                   (if (> x 0)
-                     (recur (dec x))
-                     (shift (fn [_] 2))))]
-            (g 10)))
-         2)))
 
   (testing "Error handling"
     (is (thrown?
