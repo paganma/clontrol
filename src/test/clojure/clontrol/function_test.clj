e557dd332fda6b14fc08fcba008967d5b910fd4f
Marco Paganoni <paganma@usi.ch>
2026-01-08 22:24:57 +0100

    chore(shifter): Add tests for the not-inlined arities of invoke-*

--- src/test/clojure/clontrol/function_test.clj
+++ src/test/clojure/clontrol/function_test.clj
@@ -12,8 +12,224 @@
             invoke-unknown
             invoke-shift]]))
 
-(deftest fn-cc-test
+(deftest fn-shift-test
   (testing "Multiple Arities"
+    ;; Strips the :inline tag
+    (let [invoke-shift invoke-shift
+          invoke-unknown invoke-unknown]
+      (is (= (invoke-shift
+              (fn-shift [x] x)
+              identity 1)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y] x)
+              identity 1 2)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z] x)
+              identity 1 2 3)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a] x)
+              identity 1 2 3 4)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b] x)
+              identity 1 2 3 4 5)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c] x)
+              identity 1 2 3 4 5 6)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d] x)
+              identity 1 2 3 4 5 6 7)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e] x)
+              identity 1 2 3 4 5 6 7 8)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f] x)
+              identity 1 2 3 4 5 6 7 8 9)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f g] x)
+              identity 1 2 3 4 5 6 7 8 9 10)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f g h] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f g h i] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11 12)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f g h i l] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11 12 13)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x] x)
+              identity 1)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y] x)
+              identity 1 2)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z] x)
+              identity 1 2 3)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a] x)
+              identity 1 2 3 4)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b] x)
+              identity 1 2 3 4 5)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c] x)
+              identity 1 2 3 4 5 6)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d] x)
+              identity 1 2 3 4 5 6 7)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e] x)
+              identity 1 2 3 4 5 6 7 8)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f] x)
+              identity 1 2 3 4 5 6 7 8 9)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f g] x)
+              identity 1 2 3 4 5 6 7 8 9 10)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f g h] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f g h i] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11 12)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f g h i l] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11 12 13)
+             1))
+      (is (= (invoke-shift
+              (fn-shift [x y z a b c d e f g h i l p1 p2 p3 p4 p5 p6] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x] x)
+              identity 1)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y] x)
+              identity 1 2)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z] x)
+              identity 1 2 3)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a] x)
+              identity 1 2 3 4)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a b] x)
+              identity 1 2 3 4 5)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a b c] x)
+              identity 1 2 3 4 5 6)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a b c d] x)
+              identity 1 2 3 4 5 6 7)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a b c d e] x)
+              identity 1 2 3 4 5 6 7 8)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a b c d e f] x)
+              identity 1 2 3 4 5 6 7 8 9)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a b c d e f g] x)
+              identity 1 2 3 4 5 6 7 8 9 10)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a b c d e f g h] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a b c d e f g h i] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11 12)
+             1))
+      (is (= (invoke-unknown
+              (fn-shift [x y z a b c d e f g h i l] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11 12 13)
+             1))
+      (is (= (invoke-unknown
+              (fn [x] x)
+              identity 1)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y] x)
+              identity 1 2)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z] x)
+              identity 1 2 3)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a] x)
+              identity 1 2 3 4)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a b] x)
+              identity 1 2 3 4 5)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a b c] x)
+              identity 1 2 3 4 5 6)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a b c d] x)
+              identity 1 2 3 4 5 6 7)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a b c d e] x)
+              identity 1 2 3 4 5 6 7 8)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a b c d e f] x)
+              identity 1 2 3 4 5 6 7 8 9)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a b c d e f g] x)
+              identity 1 2 3 4 5 6 7 8 9 10)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a b c d e f g h] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a b c d e f g h i] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11 12)
+             1))
+      (is (= (invoke-unknown
+              (fn [x y z a b c d e f g h i l] x)
+              identity 1 2 3 4 5 6 7 8 9 10 11 12 13)
+             1))))
+  (testing "Multiple Arities Inlined"
     (is (= (invoke-shift
             (fn-shift [x] x)
             identity 1)
@@ -318,7 +534,7 @@
          clojure.lang.ExceptionInfo
          (eval `((fn-shift [p1# p2# p3#]) 1 2 3))))))
 
-(deftest defn-cc-test
+(deftest defn-shift-test
   (testing "Well formed functions"
     (is (macroexpand `(defn-shift f# [x#] x#)))
     (is (macroexpand `(defn-shift f# [~'&form ~'&env x#] x#))))
